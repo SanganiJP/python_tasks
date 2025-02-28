6f5a51a352224c838e5d4dc5c0b5b0a9393ca8dd
sanganiJP <sanganijp212@gmail.com>
2025-02-28 14:58:50 +0530

    [Update]

--- python_test_task2.py
+++ python_test_task2.py
@@ -3,16 +3,13 @@ numbers = [4, 5, 6, 4, 7, 8, 5, 9, 4, 7, 6, 10]
 print("Duplicate Numbers and Their Counts:")
 count = {}
 for number in numbers:
-    # x = numbers.count(number)
-    if number in count:
-        count[number] += 1
-    else:
-        count[number] = 1
-
-
-# for key,val in count.items():
-#     if val == 1:
-#         count.pop(key)
+    x = numbers.count(number)
+    if x > 1:
+        count[number] = x
+    # if number in count:
+    #     count[number] += 1
+    # else:
+    #     count[number] = 1
 
 for number,count_of_number in count.items():
     print(f"{number} appears {count_of_number} times")
